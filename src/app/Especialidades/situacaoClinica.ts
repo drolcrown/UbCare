eaab278f6d012e668bd3c3dee0263148b5640aa3
Rafael Souza <rafa.jones@hotmail.com>
2018-09-23 17:13:28 -0300

    especialidades

---
+++ src/app/Especialidades/situacaoClinica.ts
@@ -0,0 +1,34 @@
+export class SituacaoClinica{
+    situacaoClinica = [
+        "cuidado paliativo",
+        "constipação",
+        "disfunção cognitiva",
+        "doença cardiovascular",
+        "doenças infectocontagiosas",
+        "doença osteomuscular",
+	    "doença respiratoria",
+        "feridas",
+        "imobiliddade",
+        "infecção trato urinario",
+        "neoplasia",
+        "pós cirurgico",
+        "pré cirurgico",
+		"reabilitação neurofunional e motora",
+		"reabilitação nutricional",
+		"sindrome da fragilidade",
+		"sindrome neurologica da infancia ao envelhecimento",
+		"transtorno saude mental",
+		"(opção descrever e escrever o profissional)"
+                
+                ];
+    areaDeAtuacao = [
+        "cuidados paliativos preventivos(acamados, pós / pré - cirúrgicos)",
+        "habilitação e desempenho(atletas)",
+        "personal trainer (acompanhamento ginásio)",
+		"prevencao doença e dores osteomusculares e articulares",
+		"prevenção contra queda", 
+	    "precenção contra doenças infectocontagiosas",
+        "treinamento para prova física"
+		
+    ]
+}
